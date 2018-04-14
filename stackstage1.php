6570817eebabb76f93f84e0e593773356bc05de2
akintomide007 <adebileakintomide@gmail.com>
2018-04-14 09:16:08 +0100

    Add files via upload

---
+++ stackstage1.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>slackstage1</title>
+<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
+<link type="text/css" rel="stylesheet" href="css/style1.css" />
+
+</head>
+
+<body>
+
+
+<div style="background-image: url(img/alice-achterhof-85968-unsplash.jpg); position: absolute; left: 0; top: 0; right: 0; bottom: 0; z-index: -1; background-position: center; background-size: cover; background-attachment: fixed;">
+			
+</div>
+		
+<div style="position: absolute; left: 0px; right: 0px; top: 10%; text-align: center; font-family: 'Montserrat', sans-serif; font-weight: 150; margin-top: 0px; margin-bottom: 20px; color: black; font-size: 96px;">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-10 col-md-offset-1">
+						<div class="home-content">
+							
+						<h22>THINK</h22>
+							<h3><span style="color: #F63419; font-size: 48px;">LAKE</span> arts</h3>
+							
+							<h3>We are a team of professionals</h3>
+						
+						</div>
+					</div>
+			
+				</div>
+			</div>
+		</div>
+	
+		
+			<div style="position: absolute; width: 434px; height: 85px; margin-left: 37%; top: 534px; text-align: center; font-family: 'Montserrat', sans-serif; font-weight: 150; margin-top: 0px; margin-bottom: 20px; color: black; font-size: 96px; left: 2px;">
+			
+		
+				<div style="width: 100%; margin-left: 47px;">
+				
+					<img class="img-responsive" src="img/stack.png" alt="">
+				</div>
+</div>
+	<div style="position: absolute; width: 524px; height: 85px; margin-left: 37%; top: 736px; text-align: center; font-family: 'Montserrat', sans-serif; font-weight: 100; margin-top: 0px; margin-bottom: 20px; color: black; font-size: 24px;">
+	<?php
+		date_default_timezone_set("Africa/Libreville");
+		echo "<h4>Time :</h4>" . date("h:ia") ;
+echo "<h4>Date :</h4>" . date("d/m/Y") ;
+
+?>
+	</div>	
+</body>
+</html>
